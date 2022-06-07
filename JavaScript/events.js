c04fd8a3a072e28a78c1f439529c3453a06cf3b7
Skaduvee <2093671@students.wits.ac.za>
2022-06-07 17:01:28 +0200

    Adding updated files

---
+++ JavaScript/events.js
@@ -0,0 +1,24 @@
+//event handling for the nav bar
+
+const navBarLinks = document.querySelectorAll("#redirect_link");
+var href_links = ["index.html", "blog.html", "design.html", "portfolio.html", "contact.html"];
+
+function CheckHTMLPage() {
+    if (location.href == "http://127.0.0.1:5500/index.html") {href_links = ["index.html", "Blogs/blog.html", "Design/design.html", "Portfolio/portfolio.html", "Contact/contact.html"];}
+    if (location.href == "http://127.0.0.1:5500/Blogs/blog.html") {href_links = ["../index.html", "blog.html", "../Design/design.html", "../Portfolio/portfolio.html", "../Contact/contact.html"];}
+    if (location.href == "http://127.0.0.1:5500/Design/design.html") {href_links = ["../index.html", "../Blogs/blog.html", "design.html", "../Portfolio/portfolio.html", "../Contact/contact.html"];}
+    if (location.href == "http://127.0.0.1:5500/Portfolio/portfolio.html") {href_links = ["../index.html", "../Blogs/blog.html", "../Design/design.html", "portfolio.html", "../Contact/contact.html"];}
+    if (location.href == "http://127.0.0.1:5500/Contact/contact.html") {href_links = ["../index.html", "../Blogs/blog.html", "../Design/design.html", "../Portfolio/portfolio.html", "contact.html"];}
+}
+
+function ChangePageOnNavBarClick() {
+    navBarLinks[0].addEventListener("click", e => location.href = href_links[0]);
+    navBarLinks[1].addEventListener("click", e => location.href = href_links[1]);
+    navBarLinks[2].addEventListener("click", e => location.href = href_links[2]);
+    navBarLinks[3].addEventListener("click", e => location.href = href_links[3]);
+    navBarLinks[4].addEventListener("click", e => location.href = href_links[4]);
+    }
+    
+CheckHTMLPage();
+ChangePageOnNavBarClick();
+/////////////////////////////////////////
