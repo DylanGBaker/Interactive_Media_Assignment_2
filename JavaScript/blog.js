3b273fc84c3a1a321f985c29fc908bda0fbf35f8
Skaduvee <2093671@students.wits.ac.za>
2022-06-09 01:04:14 +0200

    Bug fix

--- JavaScript/blog.js
+++ JavaScript/blog.js
@@ -6,11 +6,11 @@ function GetNewPageLocation(i) {
 }
 
 function ChangePageOnClick() {
-    navBarLinks[0].addEventListener("click", GetNewPageLocation(0));
-    navBarLinks[1].addEventListener("click", GetNewPageLocation(1));
-    navBarLinks[2].addEventListener("click", GetNewPageLocation(2));
-    navBarLinks[3].addEventListener("click", GetNewPageLocation(3));
-    navBarLinks[4].addEventListener("click", GetNewPageLocation(4));
+    navBarLinks[0].addEventListener("click", e=> location.href = hrefs[0]);
+    navBarLinks[1].addEventListener("click", e=> location.href = hrefs[1]);
+    navBarLinks[2].addEventListener("click", e=> location.href = hrefs[2]);
+    navBarLinks[3].addEventListener("click", e=> location.href = hrefs[3]);
+    navBarLinks[4].addEventListener("click", e=> location.href = hrefs[4]);
 }
 
 function RunEvents() {
