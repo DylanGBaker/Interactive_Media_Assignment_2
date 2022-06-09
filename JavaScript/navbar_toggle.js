aa6268cc4bf2a7f36b4c039e3ace7a022854801f
Skaduvee <2093671@students.wits.ac.za>
2022-06-09 17:10:29 +0200

    Updated nav bar for smaller form factors

--- JavaScript/navbar_toggle.js
+++ JavaScript/navbar_toggle.js
@@ -1,11 +1,9 @@
-function DirectUserToClickedLink() {
-    nav.classList.toggle("toggle_nav");
-}
-
 function NavSlide() {
     const hamBurger = document.querySelector(".Hamburger_Logo");
     const nav = document.querySelector("#hyperlinks ul");
-    hamBurger.addEventListener("click", DirectUserToClickedLink());
+    hamBurger.addEventListener("click", e => {
+         nav.classList.toggle("toggle_nav");
+    });
 }
 
 function RunEvents() {
